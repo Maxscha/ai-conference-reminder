65a654fec9d3d87b3b6d1f6326cfe1ea6dca7420
Maximilian Schall <Maximilian.Schall@mail.de>
2023-03-06 17:50:32 +0100

    Updates

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const slack = require("./src/slack");
 
 const fs = require('fs');
 
-function Sleep(milliseconds) {
-  return new Promise(resolve => setTimeout(resolve, milliseconds));
- }
+
+ 
 
 async function main() {
   //TODO make proper async
   try {
-
     const messageFilePath = 'conferences.json'
     
     let rawdata = fs.readFileSync(messageFilePath);
@@ -33,62 +31,24 @@ async function main() {
     });
 
 
-    text = "Hey, here your weekly reminder for the next upcoming AI-conferenes:\n";
+    text = "He Everyoney, \nhere is your weekly reminder for upcoming AI-conferences:\n\n";
 
     for (let conference of conferences) {
         // const deadline = conference.deadline.toLocaleDateString("de-DE");
+        
         const days = Math.ceil((conference.deadline - now) / (1000 * 60 * 60 * 24));
 
-
-        text += conference.name + " " + conference.deadline + "in " +days +  "\n";
+        //format date nicely
+        const deadline = conference.deadline.toLocaleDateString("en-en", {year: 'numeric', month: 'long', day: 'numeric'});
+        text += "**" + conference.name + "**" + " " + deadline + "in " + days +  " days\n\n";
     }
 
     text += "Feel free to add your own conferences to the list: https://github.com/Maxscha/ai-conference-reminder \n"
 
 
 
-    slack.postMessage(userToken, {"channel": "C04MCHG1P5Y", "text": text});
+    await slack.postMessage(userToken, {"channel": "C04MCHG1P5Y", "text": text});
     
-    for (let conference of conferences) {
-        // 
-    }
-
-
-    // const allMessages = buildRepeatMessages(messages);
-
-    // setup.deleteAllScheduledMessages(userTokens);
-
-    // const results = [];
-
-    // for (let message of allMessages) {
-    //   const user = message.user || "default";
-    //   const channels = userChannels[user];
-    //   const token = userTokens[user];
-    //   const messageBuilded = buildMessage(
-    //     convertChannelNameToId(message.channel, channels),
-    //     message.text,
-    //     message.post_at
-    //   );
-    //   const result = slack.sendMessage(token, messageBuilded);
-    //   result.catch((error) => {
-    //     console.error(`${error} for mesage: \n ${message.text}`);
-    //   });
-    //   results.push(result);
-    //   await Sleep(1000)
-
-      //TODO put in proper error handling
-    // }
-    // TODO Output scheduled messages
-
-    // for (let result of results) {
-    //   try {
-    //     let r = await result;
-    //     console.log(r);
-    //   } catch (error) {
-    //     // console.error(error);
-    //   }
-    // }
-    await Sleep(1000)
   } catch (error) {
     core.setFailed(error);
   }
