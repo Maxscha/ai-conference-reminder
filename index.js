3d1bb1a847e54c1cf67c370c8486349f638e84de
ddz5431 <106474427+ddz5431@users.noreply.github.com>
2023-07-27 14:14:32 +0200

    feat: refactor code based on parser.js

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
+const fs = require('fs');
 const core = require('@actions/core');
-const fs = require('fs').promises;
 const { DateTime } = require('luxon');
 const slack = require('./src/slack');
+const downloadAndParseFile = require('./src/parser');
+
+const createDirIfNotExist = (dir) => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+};
+
+const mapConferences = (conferences, now) => conferences
+  .map((conference) => {
+    const dt = DateTime.fromISO(conference.deadline.replace(' ', 'T'), { zone: conference.timezone });
+    return dt.isValid ? { ...conference, deadline: dt } : null;
+  })
+  .filter((conference) => conference && conference.deadline.ts > now.ts)
+  .sort((a, b) => a.deadline - b.deadline);
+
+const composeMessage = (conferences, now) => {
+  let text = 'Hey everyone, \nhere is your weekly reminder for upcoming AI-conferences:\n\n';
+
+  for (const conference of conferences) {
+    const deadlineDateTime = DateTime.fromISO(conference.deadline, { zone: conference.timezone });
+    const diffInMilliseconds = deadlineDateTime.diff(now);
+    const days = Math.ceil(diffInMilliseconds.as('days'));
+    const deadline = deadlineDateTime.toLocaleString(DateTime.DATE_FULL);
+
+    text += `<${conference.link}|*${conference.title} ${conference.year}*> ${deadline} in *${days}* days in ${conference.location}.\n\n`;
+  }
+  text += 'Feel free to add your own conferences to the repository: https://github.com/Maxscha/ai-conference-reminder';
+  return text;
+};
 
 async function main() {
   try {
-    const messageFilePath = 'data/conferences.json';
-    const rawdata = await fs.readFile(messageFilePath, 'utf-8');
-    let conferences = JSON.parse(rawdata);
+    const dataDir = 'data';
+    const outputLocationPath = `${dataDir}/conferences.yml`;
+    const additionalConferencesPath = `${dataDir}/additional-conferences.yml`;
+    const excludedConferencesPath = `${dataDir}/conference-exclusions.yml`;
+    const parsedConferences = `${dataDir}/parsed-conferences.json`;
+
+    createDirIfNotExist(dataDir);
+
+    await downloadAndParseFile(
+      outputLocationPath,
+      additionalConferencesPath,
+      excludedConferencesPath,
+      parsedConferences,
+    );
+
+    let conferences = JSON.parse(fs.readFileSync(parsedConferences, 'utf8'));
 
     const userToken = core.getInput('slack-user-oauth-access-token');
     const channelId = core.getInput('slack-channel');
 
     const now = DateTime.now('Europe/Berlin');
 
-    conferences = conferences
-      .map((conference) => {
-        const dt = DateTime.fromISO(conference.deadline.replace(' ', 'T'), { zone: conference.timezone });
-        return dt.isValid ? { ...conference, deadline: dt } : null;
-      })
-      .filter((conference) => conference && conference.deadline.ts > now.ts);
+    conferences = mapConferences(conferences, now);
 
-    conferences.sort((a, b) => a.deadline - b.deadline);
+    const text = composeMessage(conferences, now);
 
-    let text = 'Hey everyone, \nhere is your weekly reminder for upcoming AI-conferences:\n\n';
-
-    for (const conference of conferences) {
-      const deadlineDateTime = DateTime.fromISO(conference.deadline, { zone: conference.timezone });
-      const diffInMilliseconds = deadlineDateTime.diff(now);
-      const days = Math.ceil(diffInMilliseconds.as('days'));
-      const deadline = deadlineDateTime.toLocaleString(DateTime.DATE_FULL);
-
-      text += `<${conference.link}|*${conference.title} ${conference.year}*> ${deadline} in *${days}* days in ${conference.location}.\n\n`;
-    }
-    text += 'Feel free to add your own conferences to the repository: https://github.com/Maxscha/ai-conference-reminder';
-
+    // console.log(text);
     await slack.postMessage(userToken, { channel: channelId, text });
   } catch (error) {
     core.setFailed(`Error during execution: ${error}`);
-    console.error(error);
+    // console.error(error);
   }
 }
 
